14e4a3cbf64a6c5a227357d99b7b1f707161c670
joaquin bianciotto <joaquinbianciotto123@hotmail.com>
2020-06-09 04:00:54 -0300

    add the illumination of the sequence

--- SD.js
+++ SD.js
@@ -1,30 +1,60 @@
-const celeste = document.getElementById('celeste')
-const violeta = document.getElementById('violeta')
-const naranja = document.getElementById('naranja')
-const verde = document.getElementById('verde')
-const btnEmpezar = document.getElementById('btnEmpezar')
+const lightblue = document.getElementById('lightblue')
+const violet = document.getElementById('violet')
+const orange = document.getElementById('orange')
+const green = document.getElementById('green')
+const btnstart = document.getElementById('btnstart')
 
-class Juego {
+class game {
     constructor() {
         this.initialize()
         this.generateSequence()
+        this.nextLevel()
     }
 
-    initialize() {
-        btnEmpezar.classList.add('hide')
-        this.nivel = 1
+    initialize() {              
+        btnstart.classList.add('hide')
+        this.level = 1
         this.colores = {
-            celeste,
-            violeta,
-            naranja,
-            verde
+            lightblue,
+            violet,
+            orange,
+            green
         }
     }
-    generateSequence(){
-        this.secuencia = new Array(10).fill(0).map(n => Math.floor(Math.random()*5))
+    generateSequence(){     //generate a random color sequence
+        this.sequence = new Array(10).fill(0).map(n => Math.floor(Math.random()*4))
+    }
+
+    nextLevel(){
+        this.illuminateSequence()
+    }
+    transformColors(number){        //transform one number on sequence in a color of our board 
+        switch(number){
+            case 0:
+                return "lightblue"
+            case 1:
+                return "violet"
+            case 2:
+                return "orange"
+            case 3:
+                return "green"
+        }
+    }
+    illuminateSequence(){
+        for (let i = 0; i < this.level; i++){
+            const color = this.transformColors(this.sequence[i])
+            setTimeout(() => this.illuminateColor(color), 1500 * i) 
+        }
+    }
+    illuminateColor(color){
+        this.colores[color].classList.add("light")
+        setTimeout(() => this.turnOff(color), 1500)
+    }
+    turnOff(off){
+        this.colores[off].classList.remove("light")
     }
 }
 
-function empezarJuego() {                 
-    window.juego = new Juego()
+function startGame() {                //start the game    
+    window.game = new game()
 }
